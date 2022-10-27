12b576e0aed4445654261892fcc1345d958435c1
sravyakota <crazysravs577@gmail.com>
2022-10-27 13:17:46 +0000

    blog

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,69 @@
+import {Component} from 'react'
+
+import './index.css'
+
+// const blogData = {
+//   title: 'Blog Name',
+//   imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-3-img.png',
+//   avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+//   author: 'Author Name',
+//   content:
+//     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+// }
+
+class BlogItemDetails extends Component {
+  state = {blogData: {}}
+
+  componentDidMount() {
+    this.getBlockItemData()
+  }
+
+  getBlockItemData = async () => {
+    console.log(this.props)
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    console.log(match)
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+    console.log(data)
+    const updatedData = {
+      title: data.title,
+      imageUrl: data.image_url,
+      avatarUrl: data.avatar_url,
+      author: data.author,
+      content: data.content,
+      id: data.id,
+      topic: data.topic,
+    }
+    console.log(updatedData)
+    this.setState({blogData: updatedData})
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogData} = this.state
+
+    console.log(blogData)
+
+    const {title, imageUrl, content, avatarUrl, author} = blogData
+    return (
+      <div className="blog-info">
+        <h2 className="blog-details-title">{title}</h2>
+
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    return <div className="blog-container">{this.renderBlogItemDetails()}</div>
+  }
+}
+
+export default BlogItemDetails
